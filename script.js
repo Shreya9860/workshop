a198633f7688343b6d991e22a7b43d0898b99bd2
Your Name <you@example.com>
2024-10-11 12:41:03 +0530

    shreya

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-// document.getElementById("title").innerHTML = "New Title";
-// const a = document.getElementById("myDIV");
-const button = document.querySelector(".checked");
-console.log(button);
+// // document.getElementById("title").innerHTML = "New Title";
+// // const a = document.getElementById("myDIV");
+// const button = document.querySelector(".checked");
+// console.log(button);
 
-var close = document.getElementsByClassName("close");
-var i;
-for(i=0;i<close,length;i++)
-{
-    close[i].oneclick = function()
+// var close = document.getElementsByClassName("close");
+// var i;
+// for(i=0;i<close,length;i++)
+// {
+//     close[i].oneclick = function()
+//     {
+//         var div = this.parentElement;
+//         div.style.display = "none";
+//     };
+// }
+// var list= document.querySelector("ul");
+// list addEventListener(
+//     "click",
+//     function(ev)
+//     {
+//         if(ev.target.tagName ==="LI"){
+//             ev.target.classList.toggle("checked"); 
+//         }
+//     },
+//     false
+// );
+var list = document.querySelector("ul");
+list.addEventListener(
+    "click",
+    function (ev) {
+    if (ev.target.tagName === "LI")
     {
-        var div = this.parentElement;
-        div.style.display = "none";
-    };
+        ev.target.classList.toggle("checked");
 }
-var list=document querySelector("ul");
-list addEventListener
-{
-    "click"
-    function(ev)
-    {
-        if(ev.target.tagName ==="LI"){
-            ev.target.classList.toggle("checked"); 
-        }
-    },
-    false
-};
+},
+false
+true
+);
